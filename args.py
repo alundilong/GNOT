737cdfe92dd919eb8296277a4d0e2872229bd796
alundilong <ymao.mu@gmail.com>
2025-01-28 16:45:22 -0600

    feat:add porousmelting4d case

--- args.py
+++ args.py
@@ -12,7 +12,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='GNOT for operator learning')
     parser.add_argument('--dataset',type=str,
                         default='ns2d',
-                        choices = ['heat2d','ns2d','inductor2d','heatsink3d','ns2d_time','darcy2d','melting',])
+                        choices = ['heat2d','ns2d','inductor2d','heatsink3d','ns2d_time','darcy2d','porousmelting4d',])
 
     parser.add_argument('--openfoam', action='store_true', default=False,
                         help='cfd data with openfoam format')
