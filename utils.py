451e0c1b748295d0903757a6667030e301825325
alundilong <ymao.mu@gmail.com>
2025-01-30 10:30:48 -0600

    feat:add timing to measure performance of defined func

--- utils.py
+++ utils.py
@@ -494,4 +494,16 @@ def binterp1d(x, y, xnew, eps=1e-9):
     return ynew
 
 
+import time
+from functools import wraps
 
+def timing(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} took {elapsed_time:.4f} seconds to execute.")
+        return result
+    return wrapper
